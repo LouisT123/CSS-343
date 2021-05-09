2162e89a61605141fe711812750f010f94194e61
LouisT123 <louistaing@aol.com>
2021-05-08 20:38:53 -0700

    sanity -50, working graphm

---
+++ Assignment3/Assignment3/graphm.cpp
@@ -0,0 +1,208 @@
+//file: graphm.cpp
+//Class graphm implementations from graphm.h
+//Louis Taing
+
+#include "graphm.h"
+#include <climits>
+#include <iomanip>
+
+//-----------------------------------------------------------------------------
+//constructor
+GraphM::GraphM()
+{
+	size = 0;  //amount of nodes is 0
+	//initialize tables C and T (nested for loop for each ordered pair)
+	for (size_t i = 0; i < MAXNODES; i++)
+	{
+		for (size_t j = 0; j < MAXNODES; j++)
+		{
+			C[i][j] = INT_MAX;					//all C to "inf"
+			T[i][j].visited = false;			//all visited to false
+			T[i][j].path = 0;					//all paths to 0
+			T[i][j].dist = INT_MAX;				//all dist to "inf"
+		}
+	}
+
+}
+//-----------------------------------------------------------------------------
+//destructor
+GraphM::~GraphM()
+{
+}
+//-----------------------------------------------------------------------------
+//builds up graph node information and adjacency matrix of edges between each
+//node reading from a data file.
+void GraphM::buildGraph(ifstream& file)
+{
+	int from = 0;
+	int to = 0;
+	int weight = 0;
+
+	//extract size of graph
+	file >> size;
+	//node name
+	string nodeName = "";
+	//read line
+	getline(file, nodeName);
+	//for size of data arr, name each node
+	for (size_t i = 1; i <= size; i++)
+	{
+		//basically calls getline in nodedata.cpp
+		data[i].setData(file);
+	}
+
+	while (file >> from >> to >> weight)
+	{
+		if (from == 0)
+		{
+			break;
+		}
+		C[from][to] = weight;
+	}
+}
+//-----------------------------------------------------------------------------
+//insert an edge b/w 2 given nodes
+void GraphM::insertEdge(int from, int to, int weight)
+{
+	if (from > size || to > size)
+	{
+		return;
+	}
+	else if (from != 0 && to != 0)
+	{
+		C[from][to] = weight;
+	}
+}
+//-----------------------------------------------------------------------------
+//remove an edge b/w 2 given nodes
+void GraphM::removeEdge(int from, int to)
+{
+	C[from][to] = INT_MAX;
+}
+//-----------------------------------------------------------------------------
+//find the shortest path between every node to every other node in the
+//graph, i.e., TableType T is updated with shortest path information (Dijkstra)
+void GraphM::findShortestPath()
+{
+	int sshort = 0;
+		
+	for (int from = 1; from <= size; from++) 
+	{
+		// Distance of source vertex from itself is always 0
+		T[from][from].dist = 0;
+		// Find shortest path for all vertices
+		for (int j = 1; j <= size; j++) 
+		{
+			int curMin = INT_MAX;
+			for (int min = 1; min <= size; min++) //find shortest
+			{ 
+				//Pick the minimum distance vertex from the set of vertices not
+				// yet processed.
+				if (T[from][min].dist < curMin && !(T[from][min].visited)) 
+				{
+					curMin = T[from][min].dist;
+					sshort = min;
+				}
+			}
+			//mark picked vertex as processed 
+			T[from][sshort].visited = true;
+
+			//Update dist value of the adjacent vertices of the picked vertex.
+			for (int i = 1; i <= size; i++) {
+				//if not visited and not max vert
+				if (!T[from][i].visited && C[sshort][i] != INT_MAX &&
+					(T[from][i].dist > (T[from][sshort].dist + C[sshort][i])))
+				{
+					//add to dist, update shortest path
+					T[from][i].dist = T[from][sshort].dist + C[sshort][i];
+					T[from][i].path = sshort;
+				}
+			}
+		}
+	}
+}
+//-----------------------------------------------------------------------------
+//uses couts to demonstrate that the algorithm works properly. 
+void GraphM::displayAll()
+{
+	if (size > 0) 
+	{
+		cout << "Description      From node   To node    Dijkstra's   Path\n";
+		//2 for loops to print number, path and distance
+		for (int i = 1; i <= size; i++)
+		{
+			cout << data[i - 1] << endl;
+			for (int j = 1; j <= size; j++)
+			{
+				if (T[i][j].dist != 0) 
+				{
+					cout << setw(23) << i << setw(9) << j; 
+					//print if valid, call helper to display path
+					if (T[i][j].dist == INT_MAX)
+					{
+						cout << setw(13) << "----" << endl;
+					}
+					else 
+					{
+						cout << setw(13) << T[i][j].dist << setw(8);
+						getPathToDisplayAll(i, j);
+						cout << endl;
+					}
+				}
+			}
+		}
+		cout << endl << endl; 
+	}
+}
+//-----------------------------------------------------------------------------
+//helper for display all func
+void GraphM::getPathToDisplayAll(int from, int to)								
+{
+	if (T[from][to].dist != INT_MAX)
+	{
+		int path = to; 
+		getPathToDisplayAll(from, T[from][to].path); 
+		cout << path << " ";   
+	}
+}
+//-----------------------------------------------------------------------------
+//uses couts to display the shortest distance with path info between the 
+//fromNode to toNode.
+void GraphM::display(int from, int to)											//PROBLEM WITH DISPLAY helper i think
+{
+	//destination and origin validity
+	if ((from > size || from < 0) || (to > size || to < 0))
+	{
+		cout << setw(7) << from << setw(7) << to;
+		cout << setw(12) << "---" << endl;
+		return;
+	}
+	cout << setw(7) << from << setw(7) << to;   
+	//if able, get path to display
+	if (T[from][to].dist != INT_MAX) 
+	{   
+		cout << setw(12) << T[from][to].dist << setw(12);
+		getPathToDisplayAll(from, to); 
+		cout << endl;
+		getPathToDisplay(from, to); 
+	}
+	else
+	{
+		cout << setw(12) << "---" << endl;
+		
+	}
+	cout << endl;
+	
+
+}
+//-----------------------------------------------------------------------------	//Not working prob? //issue most likely lies here, incompatibility
+//helper for display func
+void GraphM::getPathToDisplay(int from, int to)
+{
+	if (T[from][to].dist != INT_MAX)
+	{
+		int path = to;
+		getPathToDisplay(from, T[from][to].path); 
+		cout << data[path] << endl << endl;
+	}
+}
--- Assignment3/Assignment3/graphm.h
+++ Assignment3/Assignment3/graphm.h
@@ -1,1 +1,49 @@
+//file: graphm.h
+//Class graphm class
+//Louis Taing
 #pragma once
+#include "nodedata.h"
+using namespace std;
+
+
+//---------------------------------------------------------------------------
+// Graphm class:  
+//   --allows user to find the shortest path using dijkstra given a file
+//	 --allows user to insert an edge b/w 2 nodes
+//	 --allows user to remove an edge b/w 2 nodes
+// Implementation and assumptions:
+//   -- implemented using Nodes and NodeData 
+//---------------------------------------------------------------------------
+
+//global constant
+const int MAXNODES = 101;
+
+class GraphM
+{
+public:
+	GraphM();
+	~GraphM();
+	void buildGraph(ifstream&);
+	void insertEdge(int, int, int);
+	void removeEdge(int, int);
+	void findShortestPath();
+	void displayAll();
+	void display(int, int);
+	//some utility func's may be needed
+	void getPathToDisplayAll(int, int);
+	void getPathToDisplay(int, int);
+
+private:
+	struct TableType {
+		bool visited; // whether node has been visited
+		int dist; // shortest distance from source known so far
+		int path; // previous node in path of min dist
+	};
+	NodeData data[MAXNODES];
+	int C[MAXNODES][MAXNODES]; // Cost array, the adjacency matrix
+	int size; // number of nodes in the graph
+	TableType T[MAXNODES][MAXNODES]; // stores visited, distance, path
+};
+
+
+
